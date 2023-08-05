5d4619efd66b19233142b3e55eeda501ad782414
Omar Ibne Alam <83441944+Omar035@users.noreply.github.com>
2023-08-05 17:44:06 +0600

    funcPrototypes.h

--- funcPrototypes.h
+++ funcPrototypes.h
@@ -1,4 +1,6 @@
 //Function prototypes:
 void addSchool(struct Space hS[], int * lastBlankPos), addPark(struct Space hS[], int * lastBlankPos), addHospital(struct Space hS[], int * lastBlankPos), listItems(struct Space hS[], int * lastBlankPos), getInfo(struct Space hS[], int * lastBlankPos), search(char string[], struct Space hS[], int * lastBlankPos);
 
-int addApart(struct Space hS[], int * lastBlankPos), floorSizeVerifier();
+int addApart(struct Space hS[], int * lastBlankPos);
+double currentFloorSizeChecker(double arr[], int limit), sizeLeft(double originalFloorSize, double currentFloorSize);
+
