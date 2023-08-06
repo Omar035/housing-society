4ade14617d07267902e47631212f99f23ac39c07
Omar Ibne Alam <83441944+Omar035@users.noreply.github.com>
2023-08-06 12:11:03 +0600

    Delete funcPrototypes.h

--- funcPrototypes.h
+++
@@ -1,5 +0,0 @@
-//Function prototypes:
-void addApart(struct Space hS[], int * lastBlankPos), addSchool(struct Space hS[], int * lastBlankPos), addPark(struct Space hS[], int * lastBlankPos), addHospital(struct Space hS[], int * lastBlankPos), listItems(struct Space hS[], int * lastBlankPos), getInfo(struct Space hS[], int * lastBlankPos), search(char string[], struct Space hS[], int * lastBlankPos), stringInputter(char destination[]);
-
-double currentFloorSizeChecker(double arr[], int limit), sizeLeft(double originalFloorSize, double currentFloorSize);
-
